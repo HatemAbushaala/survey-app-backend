69ca1434b977f59a030ee74d38d35cae6264b7ec
h.abushaala@yahoo.com <0776213065Hatem>
2023-07-20 20:25:24 +0300

    add load questions script

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,6 @@
-'use strict';
+"use strict";
+
+const preloaded_questions = require("../../preloaded_questions");
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +12,14 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+  const questions_count = await strapi.query("questions").count();
+  // if no questions available then we need to load default questions
+  if (questions_count === 0) {
+    const create_questions_promises = preloaded_questions.map((q) => {
+      return strapi.query("questions").create(q);
+    });
+    await Promise.all(create_questions_promises);
+    console.log("questions loaded successfully");
+  }
+};
---
+++ preloaded_questions.js
@@ -0,0 +1,61 @@
+const preloaded_questions = [
+  {
+    question: "What's your favorite programming language",
+    choices: [
+      {
+        text: "javascript",
+      },
+      {
+        text: "python",
+      },
+      {
+        text: "java",
+      },
+      {
+        text: "c#",
+      },
+      {
+        text: "other",
+      },
+    ],
+  },
+  {
+    question: "What's your role",
+    choices: [
+      {
+        id: 3,
+        text: "frontend developer",
+      },
+      {
+        id: 4,
+        text: "backend developer",
+      },
+      {
+        id: 14,
+        text: "fullstack developer",
+      },
+      {
+        id: 15,
+        text: "mobile developer",
+      },
+    ],
+  },
+  {
+    question: "How much experience do you have",
+    choices: [
+      {
+        text: "fresh",
+      },
+      {
+        text: "1-2 Years",
+      },
+      {
+        text: "2-5 Years",
+      },
+      {
+        text: "+5 Years",
+      },
+    ],
+  },
+];
+module.exports = preloaded_questions;
