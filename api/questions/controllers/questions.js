cd0bff90e8583ab5e53392b4124e388285f92f3e
h.abushaala@yahoo.com <0776213065Hatem>
2023-07-20 15:27:43 +0300

    create route to return random question

--- api/questions/controllers/questions.js
+++ api/questions/controllers/questions.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+const random = async (ctx) => {
+  const guest_id = ctx.request.query.guest_id;
 
-module.exports = {};
+  const result = await strapi.connections.default.raw(
+    `select id,question from questions where id not in (select question from answers where guest_id = '${guest_id}') order by  RANDOM() limit 1`
+  );
+  // there's no available questions
+  if (!result || !result[0])
+    return {
+      data: null,
+      available: false,
+    };
+
+  const question = await strapi
+    .query("questions")
+    .findOne({ id: result[0].id });
+
+  return { data: question, available: true };
+};
+module.exports = {
+  random,
+};
